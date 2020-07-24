c9d55f6a4b6bfec88bdee6f933a8f25df2b08e23
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-24 19:12:16 -0400

    Default Route

--- server/server.js
+++ server/server.js
@@ -8,4 +8,8 @@ server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
+server.get("/api", (req, res) => {
+  res.status(200).json({ api: "ONLINE" });
+});
+
 module.exports = server;
